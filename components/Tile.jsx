18838d899e80322d61b8abdd7453c7e98246b420
Jaeseong-CS <shimjs8@naver.com>
2020-04-28 23:43:51 +0900

    feat: tile component

---
+++ components/Tile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tile = (props) => {
+  const {
+    className, id, children,
+  } = props;
+
+  return (
+    <>
+      <div className={className} id={id}>
+        {children}
+      </div>
+
+      <style jsx>
+        {`
+          div {
+            border-radius: 34px;
+            box-shadow: -2px -2px 3px #60606070, 3px 3px 6px #00000070;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+Tile.defaultProps = {
+  className: '',
+  id: '',
+  children: null,
+};
+
+Tile.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  children: PropTypes.element,
+};
+
+export default Tile;
