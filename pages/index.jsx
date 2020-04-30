78c2c6ecbe50112e1c397aa5efee3796358234dc
Jaeseong-CS <shimjs8@naver.com>
2020-05-01 03:40:01 +0900

    feat: logged in page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 
 import Navigation from '../components/Navigation';
 
 const main = () => {
-  const page = [
-    {
-      name: 'Login',
-      link: 'login',
-    },
-    {
-      name: 'Join',
-      link: 'join',
-    },
-    {
-      name: 'About',
-      link: 'about',
-    },
-  ];
+  const page = Cookies.get('token')
+    ? [
+      {
+        name: 'Logout',
+        link: 'logout',
+      },
+      {
+        name: 'Profiles',
+        link: 'profiles',
+      },
+      {
+        name: 'About',
+        link: 'about',
+      },
+    ]
+    : [
+      {
+        name: 'Login',
+        link: 'login',
+      },
+      {
+        name: 'Join',
+        link: 'join',
+      },
+      {
+        name: 'About',
+        link: 'about',
+      },
+    ];
 
   return (
     <>
