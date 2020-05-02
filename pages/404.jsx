efc111df1822546e134e62ddf8d7ce01957c3f00
Jaeseong-CS <shimjs8@naver.com>
2020-05-03 07:41:44 +0900

    feat: 404 error page

---
+++ pages/404.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const about = () => (
+  <>
+    <div className="errorMessage">
+      404
+    </div>
+    <a className="gotoHome" href="/">
+      Home
+    </a>
+
+    <style jsx>
+      {`
+        .errorMessage {
+          position: absolute;
+          font-size: 150px;
+          left: 50%;
+          top: 40%;
+          transform: translate(-50%, -50%);
+          color: #2b2b2b;
+          text-shadow: 0px 0px 0px #60606070, 0px 0px 0px #00000070;
+          animation: Wildly 2s ease-in-out infinite;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+        }
+
+        .gotoHome {
+          position: absolute;
+          font-size: 60px;
+          left: 50%;
+          top: 60%;
+          transform: translate(-50%, -50%);
+          color: #2b2b2b;
+          text-shadow: -2px -2px 3px #60606070, 2px 2px 3px #00000070;
+          text-decoration: none;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+        }
+
+        @keyframes Wildly {
+          0% {
+            text-shadow: 0px 0px 0px #60606070, 0px 0px 0px #00000070;
+          }
+          50% {
+            text-shadow: -3px -3px 4px #60606070, 3px 3px 4px #00000070;
+          }
+        }
+      `}
+    </style>
+  </>
+);
+
+export default about;
