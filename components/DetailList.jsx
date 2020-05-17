ca4b0e78f8a9ef144c3e12109fc20e7ad9ea2bd4
Jaeseong-CS <shimjs8@naver.com>
2020-05-18 00:54:58 +0900

    fix: phenomenon in which elements are not properly deleted

--- components/DetailList.jsx
+++ components/DetailList.jsx
@@ -56,7 +56,7 @@ const DetailList = () => {
   const Close = () => {
     setStatus('none');
     const items = document.getElementsByClassName('detailItems');
-    for (let i = 0; i < items.length; i += 1) {
+    for (let i = items.length - 1; i >= 0; i -= 1) {
       items[i].remove();
     }
   };
