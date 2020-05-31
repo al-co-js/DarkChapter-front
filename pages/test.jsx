f2c65e8fcbdb5c0c368799f49461b73f7df2ec02
Jaeseong-CS <shimjs8@naver.com>
2020-05-31 22:31:32 +0900

    feat: test code

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
 import Navigation from '../components/New/Navigation';
+import TextBox from '../components/New/TextBox';
 
 const test = () => (
   <>
     <Navigation />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
+    <TextBox placeholder="placeholder" />
   </>
 );
 
