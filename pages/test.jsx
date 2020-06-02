c017fb398d0e45b8f8eb7e5e3d8910896a223972
Jaeseong-CS <shimjs8@naver.com>
2020-06-03 08:39:27 +0900

    chore: test code

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Button from '../components/New/Button';
+import { showModal } from '../components/New/Modal';
+import Navigation from '../components/New/Navigation';
+import TextBox from '../components/New/TextBox';
 
 const test = () => (
   <>
-    <Button className="bu">
+    <Button className="bu" onClick={() => showModal('o', 'a')}>
       Button
     </Button>
+    <Navigation />
+    <TextBox className="te" placeholder="ID" />
 
     <style jsx global>
       {`
@@ -15,6 +20,12 @@ const test = () => (
           left: 50%;
           top: 50%;
           transform: translate(-50%, -50%);
+        }
+
+        .te {
+          position: absolute;
+          left: 30px;
+          top: 50px;
         }
       `}
     </style>
