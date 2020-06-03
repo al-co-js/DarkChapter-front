6ab2e8c23ed9f5e8814db2b6c5a78bdf534613b3
Jaeseong-CS <shimjs8@naver.com>
2020-06-03 21:46:31 +0900

    chore: test code

--- pages/test.jsx
+++ pages/test.jsx
@@ -7,14 +7,51 @@ import TextBox from '../components/New/TextBox';
 
 const test = () => (
   <>
-    <Button className="bu1" onClick={() => showModal('프로필 생성에 성공했습니다', 'ok', 'success')}>
-      ok
+    <Button
+      className="bu1"
+      onClick={() => showModal('프로필 생성에 성공했습니다', 'ok', 'success', (res) => {
+        console.log(res);
+      })}
+    >
+      success
     </Button>
-    <Button className="bu2" onClick={() => showModal('프로필 생성에 성공했습니다', 'okcancel', 'success')}>
-      ok cancel
+    <Button
+      className="bu2"
+      onClick={() => showModal(
+        '프로필 생성에 실패했습니다. 다시 시도 하시겠습니까?',
+        'okcancel',
+        'error',
+        (res) => {
+          console.log(res);
+        },
+      )}
+    >
+      error
+    </Button>
+    <Button
+      className="bu3"
+      onClick={() => showModal(
+        '프로필을 정말로 생성 하시겠습니까?',
+        'okcancel',
+        'info',
+        (res) => {
+          console.log(res);
+        },
+      )}
+    >
+      info
     </Button>
     <Navigation />
-    <TextBox className="te" placeholder="ID" />
+    <TextBox
+      className="te"
+      placeholder="ID"
+      rule={(text) => {
+        if (text.length < 6) {
+          return { success: false, msg: '아이디가 너무 짧습니다.' };
+        }
+        return { success: true };
+      }}
+    />
     a
     <br />
     <br />
@@ -127,19 +164,25 @@ const test = () => (
     <br />
     <br />
     a
-
     <style jsx global>
       {`
         .bu1 {
           position: absolute;
-          left: 40%;
+          left: 25%;
           top: 50%;
           transform: translate(-50%, -50%);
         }
 
         .bu2 {
           position: absolute;
-          left: 60%;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        .bu3 {
+          position: absolute;
+          left: 75%;
           top: 50%;
           transform: translate(-50%, -50%);
         }
