a568a430fd81c3e740aebe781714373673129140
Jaeseong-CS <shimjs8@naver.com>
2020-06-03 14:57:58 +0900

    fix: add old modal

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 
 import { DetailList } from '../components/DetailList';
 import { Loading } from '../components/Loading';
-import { Modal } from '../components/New/Modal';
+import { Modal as NewModal } from '../components/New/Modal';
+import { Modal as OldModal } from '../components/Modal';
 
 const app = ({ Component, pageProps }) => (
   <>
@@ -13,7 +14,8 @@ const app = ({ Component, pageProps }) => (
       <title>Dark Chapter - 추억을 저장하다</title>
     </Head>
     <Loading />
-    <Modal />
+    <NewModal />
+    <OldModal />
     <DetailList />
     <Component {...pageProps} />
   </>
