0ebffe5877be418d65deccb7b7d9e7993fe292dc
심재성 <shimjs8@naver.com>
2020-07-02 09:47:22 +0900

    feat: new design

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,47 +1,38 @@
+import Link from 'next/link';
 import React from 'react';
 
 const notFound = () => (
   <>
     <div className="errorMessage">404</div>
-    <a className="gotoMain" href="/">
-      Main
-    </a>
+    <Link href="/">
+      <a className="gotoMain">
+        Main
+      </a>
+    </Link>
 
     <style jsx>
       {`
-        .errorMessage {
-          position: absolute;
-          font-size: 150px;
-          left: 50%;
-          top: 40%;
-          transform: translate(-50%, -50%);
-          color: #2b2b2b;
-          text-shadow: 0px 0px 0px #60606070, 0px 0px 0px #00000070;
-          animation: Wildly 2s ease-in-out infinite;
-          user-select: none;
-        }
-
-        .gotoMain {
-          position: absolute;
-          font-size: 60px;
-          left: 50%;
-          top: 60%;
-          transform: translate(-50%, -50%);
-          color: #2b2b2b;
-          text-shadow: -2px -2px 3px #60606070, 2px 2px 3px #00000070;
-          text-decoration: none;
-          user-select: none;
-        }
-
-        @keyframes Wildly {
-          0% {
-            text-shadow: 0px 0px 0px #60606070, 0px 0px 0px #00000070;
+          .errorMessage {
+            position: absolute;
+            font-size: 150px;
+            left: 50%;
+            top: 40%;
+            transform: translate(-50%, -50%);
+            color: #f26666;
+            user-select: none;
           }
-          50% {
-            text-shadow: -3px -3px 4px #60606070, 3px 3px 4px #00000070;
+
+          .gotoMain {
+            position: absolute;
+            font-size: 60px;
+            left: 50%;
+            top: 60%;
+            transform: translate(-50%, -50%);
+            color: #1ba0f2;
+            text-decoration: none;
+            user-select: none;
           }
-        }
-      `}
+        `}
     </style>
   </>
 );
