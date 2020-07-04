832b22af5f0aacf2ab2d03b5cc0eac97c135a69b
심재성 <shimjs8@naver.com>
2020-07-04 17:20:32 +0900

    feat: new design

--- pages/info.jsx
+++ pages/info.jsx
@@ -5,26 +5,33 @@ const info = () => (
     <div className="title" id="whatT">
       WHAT?
     </div>
-    <div className="detail" id="whatD">
+    <div className="detail">
       이 사이트는 추억을 저장하고, 흑역사를 기억하는 사이트입니다
       <br />
+      <br />
       누구든지 프로필을 볼 수 있고, 내용을 변경할 수 있습니다
+      <br />
       <br />
       단, 아래의 규칙을 모두 읽고 선을 넘지 않게 조심해주세요
     </div>
     <div className="title" id="ruleT">
-      RULE
+      RULE!
     </div>
-    <div className="detail" id="whatD">
+    <div className="detail">
       1. 사진을 올리면 내 안전이 보장되는가 생각하고 올린다
+      <br />
       <br />
       2. 사진을 올리면 우정을 지킬 수 있는가 생각하고 올린다
       <br />
+      <br />
       3. 기숙사 사진은 배경을 알아볼 수 없도록 편집하고 올린다
+      <br />
       <br />
       4. 사진을 지워달라는 요청이 들어오면 바로 지운다
       <br />
+      <br />
       5. 이 사이트의 주 목적은 추억을 저장하는 것이다
+      <br />
       <br />
       6. 위의 사항들이 지켜지지 않을 경우, 개발자에 의해 경고없이 삭제처리 될 수 있다
     </div>
@@ -46,12 +53,18 @@ const info = () => (
             top: 150px;
             left: 130px;
             margin-top: 30px;
+            margin-bottom: 100px;
             width: 80%;
             user-select: none;
           }
 
+          #whatT {
+            color: #1ba0f2;
+          }
+
           #ruleT {
             margin-top: 80px;
+            color: #f2811d;
           }
         `}
     </style>
